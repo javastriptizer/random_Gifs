ababa4a53b99519ca041d467f2e66ab5b01041d3
javastriptizer <65609455+javastriptizer@users.noreply.github.com>
2020-10-05 18:21:20 +0300

    Search

--- style/index.js
+++ style/index.js
@@ -1,17 +1,35 @@
+const searchId = document.getElementById('search');
+
 const randomgifs = () => {
-    return fetch('http://api.giphy.com/v1/gifs/trending?api_key=chAK8UHNPJzMaHhy37UbEjkle5NpUs6L&limit=10')
+    return fetch('http://api.giphy.com/v1/gifs/trending?api_key=chAK8UHNPJzMaHhy37UbEjkle5NpUs6L&limit=5')
         .then(res => res.json())
         .then(res => res.data);
 };
        
 function outputGif (gifs) {
+    view.innerHTML = '';
     gifs.forEach(({images}) => {
         const { url } = images.original;
         view.insertAdjacentHTML('beforeend', `<img src=${url}>`);
     });     
 };
 
-randomgifs().then(res => {
-    console.log(res);
-    outputGif(res);
+const searchGifs = (search) => {
+    return fetch(`http://api.giphy.com/v1/gifs/search?api_key=chAK8UHNPJzMaHhy37UbEjkle5NpUs6L&limit=5&q=${search}`)
+    .then(res => res.json())
+    .then(res => res.data)
+    .then(res => {console.log(res);
+    return res;
+    });
+}
+
+ randomgifs().then(outputGif);
+
+searchId.addEventListener('input', (e) => {
+    const { value } = searchId;
+    if (!value) {
+        randomgifs().then(outputGif);
+        return;
+    }
+    searchGifs(value).then(outputGif);
 });
